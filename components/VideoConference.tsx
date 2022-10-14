470d2f0eeb3526b67b0d674bd89ca598d39a95bd
Stephel <stephel@sopa4u.com>
2022-10-14 09:34:10 +0800

    remove the mic from video conference

--- components/VideoConference.tsx
+++ components/VideoConference.tsx
@@ -3,9 +3,20 @@ const roomName = '10xFocusHiveRoom'
 // const userFullName = 'Joseph Strawberry'
 
 const RenderJitsi = (props: any) => {
-    return <Jitsi containerStyle={{ width: '100%', height: '500px' }} frameStyle={{ display: 'block', width: '100%', height: '100%' }} roomName={roomName}
-      displayName={props.user.fullName}
-    />
+    return <Jitsi
+    containerStyle={{ width: '100%', height: '500px' }}
+    frameStyle={{ display: 'block', width: '100%', height: '100%' }}
+    roomName={roomName}
+    displayName={props.user.fullName}
+    config={{
+      enableNoAudioDetection: false,
+      startSilent: true,
+      startAudioMuted: 1,
+    }}
+    interfaceConfig={{
+      filmStripOnly: true 
+    }}
+  />
   }
 
 export default RenderJitsi
