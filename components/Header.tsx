5c92d6d0c76e9d37c2f6c0db7e499c275d182ebc
Stephel <stephel@sopa4u.com>
2022-11-02 11:02:41 +0800

    added feedback link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { Flex, Heading  } from "@chakra-ui/react";
+import { Button, Flex, Heading } from "@chakra-ui/react";
 
 const Header = () => (
-  <Flex shadow={'md'} justifyContent={'space-between'} p={4}>
+  <Flex shadow={"md"} justifyContent={"space-between"} p={4}>
     <div>
       <Link href="/">
-          <Heading as='h3' size='lg'>10xFocusHive</Heading >
+        <Heading as="h3" size="lg">
+          10xFocusHive
+        </Heading>
       </Link>
     </div>
-    <div>
+    <Flex alignContent={"center"}>
+      <Link target={'_blank'} href={'https://safemintlabs.convas.io/1'}>
+        <Button mr={8} color="teal.500">Send us feedback</Button>
+      </Link>
       <SignedIn>
         <UserButton
           userProfileMode="navigation"
@@ -18,7 +23,7 @@ const Header = () => (
           afterSignOutUrl="/"
         />
       </SignedIn>
-    </div>
+    </Flex>
   </Flex>
 );
 
